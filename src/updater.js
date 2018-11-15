c6c2c362c1910e1d4846a1b9e128eaf1a5c8e37a
juvirez <juvirez@gmail.com>
2018-11-15 22:45:10 +0400

    Custom check for updates with changelog

--- src/updater.js
+++ src/updater.js
@@ -1,24 +1,36 @@
 const {app, dialog, shell} = require('electron')
-const {autoUpdater} = require("electron-updater");
+const GitHub = require('github-api')
 
 app.on('ready', () => {
-    autoUpdater.on('update-available', (info) => {
-		const dmgUpdateField = info.files.find((updateFieldInfo) => {
-			return updateFieldInfo.url.endsWith('.dmg')
+	const github = new GitHub()
+	github.getRepo('juvirez', 'yandex-music-app').listReleases((_err, releases) => {
+		let lastRelease = releases.find((release) => {
+			return !release.draft
 		})
-		const dmgUrl = "https://github.com/juvirez/yandex-music-app/releases/download/v" + info.version + "/" + dmgUpdateField.url
-		dialog.showMessageBox(win, {
-			type: 'info',
-			buttons: ['cancel', 'download'],
-			defaultId: 1,
-			cancelId: 0,
-			message: "A new version of Yandex Music is available!",
-			detail: info.releaseName
-		}, (response) => {
-			if (response == 1) {
-				shell.openExternal(dmgUrl)
-			}
-		})
+		if (lastRelease.tag_name !== 'v' + app.getVersion()) {
+			let downloadUrl = getDownloadUrl(lastRelease)
+			dialog.showMessageBox(global.mainWindow, {
+				type: 'info',
+				buttons: ['cancel', 'download'],
+				defaultId: 1,
+				cancelId: 0,
+				message: "A new version of Yandex Music is available!",
+				detail: lastRelease.name + '\n\n' + lastRelease.body
+			}, (response) => {
+				if (response == 1) {
+					shell.openExternal(downloadUrl)
+				}
+			})
+		}
 	})
-	autoUpdater.checkForUpdates()
 })
+
+function getDownloadUrl(release) {
+	if (process.platform === 'darwin') {
+		return release.assets.find((asset) => {
+			return asset.name.endsWith('.dmg').browser_download_url
+		})
+	} else {
+		return release.html_url
+	}
+}
