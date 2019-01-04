460beca7def8ecab03f1689545f6b146adf9ebbb
juvirez <juvirez@gmail.com>
2019-01-04 13:20:45 +0400

    fix UnhandledPromiseRejectionWarning when Rate limit on github

--- src/features/updater.js
+++ src/features/updater.js
@@ -2,27 +2,30 @@ const {app, dialog, shell} = require('electron')
 const GitHub = require('github-api')
 
 const github = new GitHub()
-github.getRepo('juvirez', 'yandex-music-app').listReleases((err, releases) => {
-	
-	let lastRelease = releases.find((release) => {
-		return !release.draft
-	})
-	if (lastRelease.tag_name !== 'v' + app.getVersion()) {
-		let downloadUrl = getDownloadUrl(lastRelease)
-		dialog.showMessageBox(global.mainWindow, {
-			type: 'info',
-			buttons: ['cancel', 'download'],
-			defaultId: 1,
-			cancelId: 0,
-			message: "A new version of Yandex Music is available!",
-			detail: lastRelease.name + '\n\n' + lastRelease.body
-		}, (response) => {
-			if (response == 1) {
-				shell.openExternal(downloadUrl)
-			}
+github.getRepo('juvirez', 'yandex-music-app').listReleases()
+	.then(({data}) => {
+		let lastRelease = data.find((release) => {
+			return !release.draft
 		})
-	}
-})
+		if (lastRelease.tag_name !== 'v' + app.getVersion()) {
+			let downloadUrl = getDownloadUrl(lastRelease)
+			dialog.showMessageBox(global.mainWindow, {
+				type: 'info',
+				buttons: ['cancel', 'download'],
+				defaultId: 1,
+				cancelId: 0,
+				message: "A new version of Yandex Music is available!",
+				detail: lastRelease.name + '\n\n' + lastRelease.body
+			}, (response) => {
+				if (response == 1) {
+					shell.openExternal(downloadUrl)
+				}
+			})
+		}
+	})
+	.catch((error) => {
+		console.warn("Cannot fetch releases from github:", error.response.status);
+	})
 
 function getDownloadUrl(release) {
 	if (process.platform === 'darwin') {
