952864efe052c0c53ed7ccfc11d6f97011f85853
juvirez <juvirez@gmail.com>
2020-02-15 21:57:40 +0400

    ga: save and use clientId

--- src/main/features/ga.js
+++ src/main/features/ga.js
@@ -1,13 +1,24 @@
 const { app } = require("electron");
+const settings = require("electron-settings");
 const Analytics = require("electron-google-analytics");
 const analytics = new Analytics.default("UA-127383106-1");
 
+let clientId = settings.get("gaClientId");
+
 global.mainWindow.on("focus", () => {
-  analytics.screen(
-    app.name,
-    app.getVersion(),
-    "com.github.juvirez.yandexmusicapp",
-    "com.github.juvirez.yandexmusicinstaller",
-    "main"
-  );
+  analytics
+    .screen(
+      app.name,
+      app.getVersion(),
+      "com.github.juvirez.yandexmusicapp",
+      "com.github.juvirez.yandexmusicinstaller",
+      "main",
+      clientId
+    )
+    .then(response => {
+      if (!clientId) {
+        clientId = response.clientID;
+        settings.set("gaClientId", clientId);
+      }
+    });
 });
