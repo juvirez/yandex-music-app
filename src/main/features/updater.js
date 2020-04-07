948f422fc47c6a6451aa64002a40604246c90608
juvirez <juvirez@gmail.com>
2020-04-07 22:56:03 +0400

    check app version by semver (not by version equals)

--- src/main/features/updater.js
+++ src/main/features/updater.js
@@ -1,15 +1,16 @@
 const { app, dialog, shell } = require("electron");
 const GitHub = require("github-api");
+const semver = require("semver");
 
 const github = new GitHub();
 github
   .getRepo("juvirez", "yandex-music-app")
   .listReleases()
   .then(({ data }) => {
-    let lastRelease = data.find(release => {
+    let lastRelease = data.find((release) => {
       return !release.prerelease;
     });
-    if (lastRelease.tag_name !== "v" + app.getVersion()) {
+    if (semver.gt(lastRelease.tag_name, app.getVersion())) {
       let downloadUrl = getDownloadUrl(lastRelease);
       dialog.showMessageBox(
         global.mainWindow,
@@ -19,9 +20,9 @@ github
           defaultId: 1,
           cancelId: 0,
           message: "A new version of Yandex Music is available!",
-          detail: lastRelease.name + "\n\n" + lastRelease.body
+          detail: lastRelease.name + "\n\n" + lastRelease.body,
         },
-        response => {
+        (response) => {
           if (response == 1) {
             shell.openExternal(downloadUrl);
           }
@@ -29,13 +30,13 @@ github
       );
     }
   })
-  .catch(error => {
+  .catch((error) => {
     console.warn("Cannot fetch releases from github:", error.response.status);
   });
 
 function getDownloadUrl(release) {
   if (process.platform === "darwin") {
-    return release.assets.find(asset => {
+    return release.assets.find((asset) => {
       return asset.name.endsWith(".dmg");
     }).browser_download_url;
   } else {
