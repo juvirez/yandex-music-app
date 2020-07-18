37ce1114f285a55ea7aa252a04497782cb8703d0
juvirez <juvirez@gmail.com>
2020-07-18 22:37:34 +0400

    electron 8.4.0 and block new WebRTC connection
    This reverts commit d356a4e351a48445caa24197d75a00414a7f962f.

--- src/main/features/sessionModifier.js
+++ src/main/features/sessionModifier.js
@@ -3,7 +3,7 @@ const { session } = require("electron");
 // Yandex Metrica uses RTCPeerConnection, which triggered a MacOS warning "accept incoming network connections?"
 // Just block it
 session.defaultSession.webRequest.onBeforeRequest(
-  { urls: ["https://mc.yandex.ru/metrika/watch.js*"] },
+  { urls: ["https://mc.yandex.ru/metrika/*"] },
   (_details, callback) => {
     callback({ cancel: true });
   }
