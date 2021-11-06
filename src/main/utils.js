ef42120e267f5187134c69513548baa208700f0d
juvirez <juvirez@gmail.com>
2021-11-06 23:08:52 +0400

    fixed debounce function

--- src/main/utils.js
+++ src/main/utils.js
@@ -1,12 +1,11 @@
+let timeout;
+
 exports.debounce = function(func, wait) {
-	let timeout;
-	return function() {
-		let context = this;
-		let later = function() {
-			timeout = null;
-			func.apply(context, arguments);
-		};
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
+	let context = this;
+	let later = function() {
+		timeout = null;
+		func.apply(context, arguments);
 	};
+	clearTimeout(timeout);
+	timeout = setTimeout(later, wait);
 };
