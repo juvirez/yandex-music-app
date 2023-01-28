0b8b75707a2d7b9337b9e418afaae9e8d441aec0
nikk gitanes <tsynik@gmail.com>
2023-01-28 10:32:14 +0300

    revert openURL window size to original
    it was local build bug with shown scrollbars which broke UI

--- src/main/dialogs/openURL.js
+++ src/main/dialogs/openURL.js
@@ -37,7 +37,7 @@ ipcMain.on("navigate", (_event, url) => {
 
 function createWindow() {
   let win = new BrowserWindow({
-    width: 420,
+    width: 404,
     height: 116,
     modal: true,
     parent: global.mainWindow,
